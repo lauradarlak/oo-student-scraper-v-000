eb9da03303e4fca019914c88a1f38a30acb81d4b
Laura Darlak <laura@polyporedesign.com>
2018-09-14 16:39:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,17 @@ class Scraper
     # Opens a file and reads it into a variable
     html = File.read('fixtures/student-site/index.html')
 
+    student_index = Nokogiri::HTML(html)
+
+    students = {}
+
+    # Iterate through the students
+
+    binding.pry
+
   end
+
+  self.scrape_index_page(index_url)
 
   def self.scrape_profile_page(profile_url)
 
