863cf3a55cb07da8e42e17533c02fe92f2e8c867
Laura Darlak <laura@polyporedesign.com>
2018-09-14 18:07:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,18 +12,22 @@ class Scraper
   def self.scrape_index_page(index_url)
     # Opens a file and reads it into a variable
     html = File.read('fixtures/student-site/index.html')
-
     student_index = Nokogiri::HTML(html)
 
-    # students = {}
+
 
     # Iterate through the students
-
+    student_index.css("div.student-card").each do |student|
+      students = {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text
+        :profile_url => ("a").text
+      }
+    end
+    students
     binding.pry
 
   end
-
-
 
   def self.scrape_profile_page(profile_url)
 
