d8b04ae3d023d49a527e1ec146b332c02ffed978
Laura Darlak <laura@polyporedesign.com>
2018-09-14 19:44:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, data|
-      binding.pry
+      self.send(("#{attribute}="), data)
     end
   end
 
