ea9faa75029be8604307daa7df4b73a7a5c047a5
Laura Darlak <laura@polyporedesign.com>
2018-09-14 18:15:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
       students = {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
-        :profile_url => student.css("a").attribute["href"].value
+        :profile_url => student.css("a").attribute("href").value
       }
 
     end
