1086ea0830fd5bdb9880aad312371347b4bd134b
Laura Darlak <laura@polyporedesign.com>
2018-09-14 18:31:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,16 +23,14 @@ class Scraper
         :location => student.css("p.student-location").text,
         :profile_url => student.css("a").attribute("href").value
       }
-
     end
 
     students
 
-
   end
 
   def self.scrape_profile_page(profile_url)
-
+    student_profile = Nokogiri::HTML(File.read(profile_url))
   end
 
 end
