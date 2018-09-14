735a1fa0e12829dc6c35df69d3bac3da0f857a72
Laura Darlak <laura@polyporedesign.com>
2018-09-14 17:36:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+# students: student_index.css("div.student-card")
+# name: student.css("h4.student-name").text
+# location: student.css("p.student-location").text
+
+
 class Scraper
 
   def self.scrape_index_page(index_url)
