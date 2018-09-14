83f5d1201293a5da5a310e48dc70ba94811595b8
Laura Darlak <laura@polyporedesign.com>
2018-09-14 18:23:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,11 +18,11 @@ class Scraper
 
     # Iterate through the students
     student_index.css("div.student-card").each do |student|
-      students = (
+      students = [
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
         :profile_url => student.css("a").attribute("href").value
-      )
+      ]
 
     end
 
